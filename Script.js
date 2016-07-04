4483a61af64782ce422fa498b7243b4c56c25680
jSummermers <jason.summers467@gmail.com>
2016-07-04 12:03:00 -0500

    rsvp 80%

--- Script.js
+++ Script.js
@@ -8,5 +8,37 @@ $(document).ready(function () {
         }, 'smooth');
     });
     
-    $("#")
+    $("#rsvp-send").on('click', function() {
+       var valid = false;
+       
+       valid = true;
+       console.log("sending");
+       
+       if (valid == true)
+       {
+           // Collect rsvp fields
+           var rsvp = {
+                name: $("#full-name").val(),
+                //coming: $("#").value,
+                count: $("#count").val(),
+                notes: $("#rsvp-notes").val()
+           }
+           
+           
+           
+           
+           console.log(rsvp);
+           
+            $.ajax( { url: "https://api.mlab.com/api/1/databases/one_summers_day/collections/rsvp?apiKey=ofhZUMH_83xHQ3Eq3Dd9Ji3TRZ1u5Ste",
+                data: JSON.stringify( { "x" : 1 } ),
+                type: "POST",
+                contentType: "application/json",
+                success: function(data) {
+                    alert("Thank you, We've recieved your rsvp! :)");
+                } 
+            });
+       } 
+       
+        
+    });
 });
